05fc2b6890bfac5c66946628db8dfdfa85c580dc
VodeniZeko <salexy.es@gmail.com>
2019-03-12 00:13:33 -0700

    first push

---
+++ script.js
@@ -0,0 +1,167 @@
+
+// document.addEventListener('DOMContentLoaded', function() {
+//   Typed.new('.type', {
+//     strings: ["Developer", "Designer", "Illuslator"],
+//     typeSpeed: 20,
+//     loop: null
+//   });
+// });
+
+// particlesJS("particles-js", {
+//   "particles": {
+//     "number": {
+//       "value": 60,
+//       "density": {
+//         "enable": true,
+//         "value_area": 800
+//       }
+//     },
+//     "color": {
+//       "value": "#ffffff"
+//     },
+//     "shape": {
+//       "type": "circle",
+//       "stroke": {
+//         "width": 0,
+//         "color": "#fff"
+//       },
+//       "polygon": {
+//         "nb_sides": 5
+//       },
+//       "image": {
+//         "src": "img/github.svg",
+//         "width": 100,
+//         "height": 100
+//       }
+//     },
+//     "opacity": {
+//       "value": 0.5,
+//       "random": false,
+//       "anim": {
+//         "enable": false,
+//         "speed": 1,
+//         "opacity_min": 0.1,
+//         "sync": false
+//       }
+//     },
+//     "size": {
+//       "value": 3,
+//       "random": true,
+//       "anim": {
+//         "enable": false,
+//         "speed": 40,
+//         "size_min": 0.1,
+//         "sync": false
+//       }
+//     },
+//     "line_linked": {
+//       "enable": true,
+//       "distance": 150,
+//       "color": "#ffffff",
+//       "opacity": 0.4,
+//       "width": 1
+//     },
+//     "move": {
+//       "enable": true,
+//       "speed": 6,
+//       "direction": "none",
+//       "random": false,
+//       "straight": false,
+//       "out_mode": "out",
+//       "bounce": false,
+//       "attract": {
+//         "enable": false,
+//         "rotateX": 600,
+//         "rotateY": 1200
+//       }
+//     }
+//   },
+//   "interactivity": {
+//     "detect_on": "canvas",
+//     "events": {
+//       "onhover": {
+//         "enable": true,
+//         "mode": "grab"
+//       },
+//       "onclick": {
+//         "enable": true,
+//         "mode": "push"
+//       },
+//       "resize": true
+//     },
+//     "modes": {
+//       "grab": {
+//         "distance": 140,
+//         "line_linked": {
+//           "opacity": 1
+//         }
+//       },
+//       "bubble": {
+//         "distance": 400,
+//         "size": 40,
+//         "duration": 2,
+//         "opacity": 8,
+//         "speed": 3
+//       },
+//       "repulse": {
+//         "distance": 200,
+//         "duration": 0.4
+//       },
+//       "push": {
+//         "particles_nb": 4
+//       },
+//       "remove": {
+//         "particles_nb": 2
+//       }
+//     }
+//   },
+//   "retina_detect": true
+// });
+
+
+// $(function(){
+//     $(".text").typed({
+//       strings: ["var <span class='violet'>developer</span> =  <span class='white'>\u007B^400 </span>\n <span class='violet'>firstName</span><span class='white'>:</span> <span class='green'>'Edvin'</span><span class='white'>,</span> \n <span class='violet'>lastName</span><span class='white'>:</span> <span class='green'>'Saletovic'</span><span class='white'>,</span> \n <span class='violet'>skills</span><span class='white'>:</span> \n   <span class='violet'>HTML5</span><span class='white'>:</span> true<span class='white'>,</span> \n   <span class='violet'>CSS3</span><span class='white'>:</span> true<span class='white'>,</span> \n   <span class='violet'>basicJavaScript</span><span class='white'>:</span> true<span class='white'>,</span> \n   <span class='violet'>jQuery</span><span class='white'>:</span> true<span class='white'>,</span> \n <span class='violet'>tools</span><span class='white'>:</span> \n   <span class='violet'>git</span><span class='white'>:</span> true<span class='white'>,</span> \n   <span class='violet'>gulp</span><span class='white'>:</span> true<span class='white'>,</span> \n   <span class='violet'>sass</span><span class='white'>:</span> true<span class='white'>,</span> \n   <span class='violet'>pugJade</span><span class='white'>:</span> true \n <span class='white'>\u007D</span>  ", "hire(developer)<span class='white'>;</span>^1000"],
+//       typeSpeed: 2,
+//       showCursor: true,
+//       cursorChar: "|",
+//       loop: true,
+//       loopCount: null,
+//       backSpeed: 30
+//     });
+//   });
+
+
+
+
+// var lFollowX = 0,
+//     lFollowY = 0,
+//     x = 0,
+//     y = 0,
+//     friction = 1 / 30;
+
+// function moveBackground() {
+//   x += (lFollowX - x) * friction;
+//   y += (lFollowY - y) * friction;
+  
+//   translate = 'translate(' + x + 'px, ' + y + 'px) scale(1.1)';
+
+//   $('img').css({
+//     '-webit-transform': translate,
+//     '-moz-transform': translate,
+//     'transform': translate
+//   });
+
+//   window.requestAnimationFrame(moveBackground);
+// }
+
+// $(window).on('mousemove click', function(e) {
+
+//   var lMouseX = Math.max(-100, Math.min(100, $(window).width() / 2 - e.clientX));
+//   var lMouseY = Math.max(-100, Math.min(100, $(window).height() / 2 - e.clientY));
+//   lFollowX = (20 * lMouseX) / 100; // 100 : 12 = lMouxeX : lFollow
+//   lFollowY = (10 * lMouseY) / 100;
+
+// });
+
+// moveBackground();
